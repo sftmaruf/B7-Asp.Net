1e12f744970259c78989e9c5e412d831c434c005
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-06-19 11:59:26 +0600

    Arragne the basic structure of the project

---
+++ Assignment1/IObjectToJsonConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public  interface IObjectToJsonConverter
+    {
+        public string ConvertToJson(object obj);
+        public void Print();
+    }
+}
