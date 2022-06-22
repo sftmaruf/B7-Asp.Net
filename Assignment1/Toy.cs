5d6f9af49dc6cb37984bc258a150893b84e06730
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-06-23 00:56:31 +0600

    added some dummy object to test the implemented code

---
+++ Assignment1/Toy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Toy
+    {
+        public string name;
+        public int price = 10;
+    }
+}
