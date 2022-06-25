f24ba2fa2abe55710121e9122c0831b3cbc79556
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-06-25 18:52:46 +0600

    All class implement this interface denotes, the class holds the implementation of how to convert Object to Json

---
+++ Assignment1/IJsonConvertible.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public interface IJsonConvertible
+    {
+        public void ConvertToJson(object obj, ref StringBuilder _tempString, ref bool isObjectInsideArray, ref string? objectName);
+    }
+}
