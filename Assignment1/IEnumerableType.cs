d1866520399ea7cfac892af3cfe17bbb4d245df0
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-06-25 18:58:34 +0600

    denotes all the class implement the interface must implement the code for convert an enumerable type to json

---
+++ Assignment1/IEnumerableType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public interface IEnumerableType
+    {
+        public void ConvertToJson(FieldInfo field, object? obj, ref StringBuilder _tempString, ref bool isObjectInsideArray);
+        public void ConvertToJson(PropertyInfo property, object? obj, ref StringBuilder _tempString, ref bool isObjectInsideArray);
+    }
+}
