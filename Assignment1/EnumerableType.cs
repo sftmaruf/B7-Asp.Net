9c13ebb7cf9227c91201e543e7861a6537ed18ec
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-06-25 23:08:33 +0600

    add support for dictionary types

--- Assignment1/EnumerableType.cs
+++ Assignment1/EnumerableType.cs
@@ -11,6 +11,7 @@ namespace Assignment1
     {
         private IObjectToJsonConverter _converter;
         private bool isObject(TypeInfo type) => !Type.IsJsonableType(type);
+        private bool isString(dynamic value) => value.GetType() == typeof(string);
 
         public EnumerableType(IObjectToJsonConverter _converter)
         {
@@ -20,25 +21,62 @@ namespace Assignment1
         public void ConvertToJson(FieldInfo field, object? obj, ref StringBuilder _tempString, ref bool isObjectInsideArray)
         {
             dynamic? values = field.GetValue(obj);
-            Convert(values, field.Name, ref _tempString, ref isObjectInsideArray);
+           
+            if (Type.isDictionaryType(field))
+            {
+                ConvertDictionaryTypetoJson(values, field.Name, ref _tempString);
+            }
+            else
+            {
+                ConvertToJsonArray(values, field.Name, ref _tempString, ref isObjectInsideArray);
+            }
         }
 
         public void ConvertToJson(PropertyInfo property, object? obj, ref StringBuilder _tempString, ref bool isObjectInsideArray)
         {
             dynamic? values = property.GetValue(obj);
-            Convert(values, property.Name, ref _tempString, ref isObjectInsideArray);
+
+            if (Type.isDictionaryType(property))
+            {
+                ConvertDictionaryTypetoJson(values, property.Name, ref _tempString);
+            }
+            else
+            {
+                ConvertToJsonArray(values, property.Name, ref _tempString, ref isObjectInsideArray);
+            }
         }
 
-        private void Convert(dynamic values, string name, ref StringBuilder _tempString, ref bool isObjectInsideArray)
+        private void ConvertDictionaryTypetoJson(dynamic values, string name, ref StringBuilder _tempString)
         {
-            string title = StringFormatter.WrapByQuotation(name);
+            string jsonKey = StringFormatter.WrapByQuotation(name);
 
-            _tempString.Append(title).Append(": ").Append("[ ");
-            AppendValues(values, ref _tempString, ref isObjectInsideArray);
-            _tempString.Append("],\n");
+            _tempString.Append(jsonKey).Append(": ").Append(" {").Append('\n');
+            AppendValuesToJsonDictionary(values, ref _tempString);
+            _tempString.Append("},").Append('\n');
         }
 
-        private void AppendValues(dynamic values, ref StringBuilder _tempString, ref bool isObjectInsideArray)
+        private void AppendValuesToJsonDictionary(dynamic values, ref StringBuilder _tempString)
+        {
+            dynamic? dictionaryKeys = values.Keys;
+
+            foreach (var dictionaryKey in dictionaryKeys)
+            {
+                string jsonKey = StringFormatter.WrapByQuotation(dictionaryKey);
+                string value = isString(values[dictionaryKey]) ? StringFormatter.WrapByQuotation(values[dictionaryKey].ToString()) : values[dictionaryKey].ToString();
+                _tempString.Append(jsonKey).Append(": ").Append(value).Append(',').Append("\n ");
+            }
+        }
+
+        private void ConvertToJsonArray(dynamic values, string name, ref StringBuilder _tempString, ref bool isObjectInsideArray)
+        {
+            string key = StringFormatter.WrapByQuotation(name);
+
+            _tempString.Append(key).Append(": ").Append("[ ");
+            AppendValuesToJsonArray(values, ref _tempString, ref isObjectInsideArray);
+            _tempString.Append("]").Append(",").Append("\n");
+        }
+
+        private void AppendValuesToJsonArray(dynamic values, ref StringBuilder _tempString, ref bool isObjectInsideArray)
         {
             foreach (var value in values)
             {
@@ -59,7 +97,7 @@ namespace Assignment1
             isObjectInsideArray = true;
             _tempString.Append("{ ");
             _converter.DismantleObject(item);
-            _tempString.Append("}, ");
+            _tempString.Append("},");
             isObjectInsideArray = false;
         }
     }
---
+++ Assignment1/Type.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public static class Type 
+    {
+        public static bool isDictionaryType(FieldInfo field)
+        {
+            if(isEnumerableType(field))
+            {
+                System.Type? enumerableType = field.FieldType.GetInterface("IEnumerable`1");
+                if (enumerableType is null) return false;
+                return enumerableType.GetGenericArguments().Any(t => t.Name == "KeyValuePair`2");
+            }
+            return false;
+        }
+
+        public static bool isDictionaryType(PropertyInfo property)
+        {
+            if (isEnumerableType(property))
+            {
+                System.Type? enumerableType = property.PropertyType.GetInterface("IEnumerable`1");
+                if (enumerableType is null) return false;
+                return enumerableType.GetGenericArguments().Any(t => t.Name == "KeyValuePair`2");
+            }
+            return false;
+        }
+
+        public static bool isEnumerableType(FieldInfo field)
+        {
+            return (field.FieldType.GetInterface("ICollection") != null && field.FieldType.GetInterface("IEnumerable") != null)
+                || field.FieldType.IsInterface && field.FieldType.GetInterface("IEnumerable") != null
+                || field.FieldType.IsArray;
+        }
+
+        public static bool isEnumerableType(PropertyInfo property)
+        {
+
+            return (property.PropertyType.GetInterface("ICollection") != null && property.PropertyType.GetInterface("IEnumerable") != null)
+                || property.PropertyType.IsInterface && property.PropertyType.GetInterface("IEnumerable") != null
+                || property.PropertyType.IsArray;
+        }
+
+        public static bool IsJsonableType(FieldInfo field)
+        {
+            return field.FieldType.IsValueType
+                || field.FieldType.Name == "String";
+        }
+
+        public static bool IsJsonableType(TypeInfo type)
+        {
+            return type.IsValueType
+                || type.Name == "String";
+        }
+
+        public static bool IsJsonableType(PropertyInfo property)
+        {
+            return property.PropertyType.IsValueType
+                || property.PropertyType.Name == "String";
+        }
+    }
+}
