6bdbd04b93d0902d9e8167c88c086e07d8fc585c
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-06-26 18:50:18 +0600

    Refactored and Added support for null fields

--- Assignment1/Field.cs
+++ Assignment1/Field.cs
@@ -9,9 +9,9 @@ namespace Assignment1
 {
     public class Field : IJsonConvertible
     {
-        private IObjectToJsonConverter _converter;
-        private IEnumerableType _enumerableType;
-        private bool isObject(FieldInfo field) => !Type.IsJsonableType(field);
+        private readonly IObjectToJsonConverter _converter;
+        private readonly IEnumerableType _enumerableType;
+        private static bool IsObject(FieldInfo field) => !Type.IsJsonableType(field);
 
         public Field(IObjectToJsonConverter converter, IEnumerableType enumerableType)
         {
@@ -19,7 +19,7 @@ namespace Assignment1
             _enumerableType = enumerableType;
         }
 
-        public void ConvertToJson(object obj, ref StringBuilder _tempString, ref bool isObjectInsideArray, ref string? objectName)
+        public void ConvertToJson(object obj, ref StringBuilder _json)
         {
             FieldInfo[] fields = obj.GetType().GetFields();
 
@@ -27,33 +27,45 @@ namespace Assignment1
             {
                 if (Type.IsJsonableType(field))
                 {
-                    _tempString.Append(BuildJsonString(field, obj, isObjectInsideArray));
+                    _json.Append(BuildJsonString(field, obj));
                 }
-
-                if (Type.isEnumerableType(field))
+                else if (Type.isEnumerableType(field))
                 {
-                    _enumerableType.ConvertToJson(field, obj, ref _tempString, ref isObjectInsideArray);
+                    _enumerableType.ConvertToJson(field, obj, ref _json);
                     continue;
                 }
-
-                if (isObject(field))
+                else if (IsObject(field))
                 {
-                    string title = StringFormatter.WrapByQuotation(field.Name);
-                    _tempString.Append(title).Append(": ").Append("{\n");
-                    _converter.Convert(field.GetValue(obj));
-                    _tempString.Append("},\n");
+                    string key = StringFormatter.WrapByQuotation(field.Name);
+                    var value = field.GetValue(obj);
+
+                    AppendValueToJsonObject(ref _json, key, value);
                 }
             }
         }
 
-        private string BuildJsonString(FieldInfo field, object? obj, bool isObjectInsideArray)
+        private void AppendValueToJsonObject(ref StringBuilder _json, string key, object? value)
+        {
+            _json?.Append(key).Append(": ");
+            if (value == null)
+            {
+                _json?.Append("null").Append('\n');
+            }
+            else
+            { 
+                _json?.Append('{').Append('\n');
+                _converter.Convert(value);
+                _json?.Append('}').Append(',').Append('\n');
+            }
+        }
+
+        private string BuildJsonString(FieldInfo field, object? obj)
         {
             StringBuilder stringBuilder = new StringBuilder();
-            string? value = field.FieldType == typeof(string) ? StringFormatter.WrapByQuotation(field.GetValue(obj)?.ToString())  : field.GetValue(obj)?.ToString();
-            string title = StringFormatter.WrapByQuotation(field.Name);
+            string? value = field.FieldType == typeof(string) || field.FieldType == typeof(DateTime) ? StringFormatter.WrapByQuotation(field.GetValue(obj)?.ToString())  : field.GetValue(obj)?.ToString();
+            string jsonKey = StringFormatter.WrapByQuotation(field.Name);
 
-            stringBuilder.Append(title).Append(": ").Append(value).Append(", ");
-            if (!isObjectInsideArray) stringBuilder.Append('\n');
+            stringBuilder.Append(jsonKey).Append(": ").Append(value).Append(", ").Append('\n');
             return stringBuilder.ToString();
         }
     }
