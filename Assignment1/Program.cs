d29ce0c8490515c892aebae12e48cfbcfc747cd1
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-06-26 23:44:18 +0600

    Refactored

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,18 +1,4 @@
 ﻿using Assignment1;
 
-
-TestChildObject tObject2 = new();
-tObject2.name = "property1";
-tObject2.age = 10;
-tObject2.toy = new Toy() { name = "plane" };
-
-TestObject tObject = new TestObject();
-//tObject.property1 = "Shafayet";
-//tObject.property2 = "Maruf";
-//tObject.property3 = 10;
-//tObject.property4 = 20.10;
-//tObject.property5 = 1111110;
-//tObject.t = tObject2;
-
-string? json = JsonFormatter.Convert(DummyObject.getObject());
+string? json = JsonFormatter.Convert(DummyObject.getObject(2));
 Console.WriteLine(json);
