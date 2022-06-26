b4b24b6687aae47eb3849d1d0fdcf92fee46bbb6
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-06-26 18:50:50 +0600

    Refactored and Added support for null properties

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,12 +1,18 @@
 ﻿using Assignment1;
 
+
+TestChildObject tObject2 = new();
+tObject2.name = "property1";
+tObject2.age = 10;
+tObject2.toy = new Toy() { name = "plane" };
+
 TestObject tObject = new TestObject();
-tObject.property1 = "property1";
-tObject.property2 = "property2";
+tObject.property1 = "Shafayet";
+tObject.property2 = "Maruf";
 tObject.property3 = 10;
 tObject.property4 = 20.10;
 tObject.property5 = 1111110;
+tObject.t = tObject2;
 
-IObjectToJsonConverter objectToJsonConverter = new ObjectToJsonConverter();
-objectToJsonConverter.ConvertToJson(tObject);
-objectToJsonConverter.Print();
+string? json = JsonFormatter.Convert(tObject);
+Console.WriteLine(json);
