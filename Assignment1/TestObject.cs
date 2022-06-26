b6e9add16adda9f61f9586db33c6fb7a2759b2c5
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-06-26 21:49:59 +0600

    Added IList, IDictionary, Instance, DateTime types of fields and property

--- Assignment1/TestObject.cs
+++ Assignment1/TestObject.cs
@@ -13,7 +13,11 @@ namespace Assignment1
         public int property3;
         public double property4;
         public long property5;
-        public TestObject t;
+        public IList<string> strs = new List<string>() { "sft", "maruf", null };
+        public IDictionary<string, int> data = new Dictionary<string, int>() { { "sft", 1 }, { "Maruf", 20 } };
+        public TestChildObject t;
+        public TestChildObject tProp { get; set; }
+        public DateTime date = DateTime.Now;
 
         public void TestMethod()
         {
