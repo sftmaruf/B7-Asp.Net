b3b4969a4d85a99bbd874462dc28ba327177e90a
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-06-26 19:22:24 +0600

    Refactored and separated Indentation related implementation to `CodeIndentor` class

--- Assignment1/StringFormatter.cs
+++ Assignment1/StringFormatter.cs
@@ -8,28 +8,11 @@ namespace Assignment1
 {
     public class StringFormatter
     {
-        public static StringBuilder RemoveExtraCommas(StringBuilder _json)
+        public static string WrapByQuotation(string? placeholder)
         {
-            int commaPosition = -1;
-            for (int i = 0; i < _json.Length; i++)
-            {
-                if (_json[i] == ',') commaPosition = i;
-                if (commaPosition != -1 && (_json[i] != ' ' && _json[i] != '}' && _json[i] != ']' && _json[i] != ',')) commaPosition = -1;
-                if (commaPosition != -1 && (_json[i] == '}' || _json[i] == ']'))
-                {
-                    _json = _json.Remove(commaPosition, 1);
-                    i--;
-                }
-            }
-            return _json;
-        }
-
-        public static void FormatTempString(bool isObjectType, string? objectName, ref StringBuilder _tempString)
-        {
-            if (isObjectType)
-            {
-                _tempString.Insert(0, objectName + "{\n").Append("}\n");
-            }
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append('"').Append(placeholder).Append('"');
+            return stringBuilder.ToString();
         }
     }
 }
