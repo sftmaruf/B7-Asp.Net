3be0ac192ef7ad049251f683a8e3afe559be197e
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-07-05 02:46:59 +0600

    started to solving first problem

--- Assignment2/K-th-Not-Divisible-by-n/Program.cs
+++ Assignment2/K-th-Not-Divisible-by-n/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int divisor = Convert.ToInt32(Console.ReadLine());
+int position = Convert.ToInt32(Console.ReadLine());
+
+void BinarySearch(int divisor, int position)
+{
+    int left = 0, right = position, mid = (left + right)/2;
+
+}
