8bd7deb6dd3e442a3e8def07e8f930cb9f4d96e2
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-07-06 12:39:04 +0600

    solved first problem.

--- Assignment2/K-th-Not-Divisible-by-n/Program.cs
+++ Assignment2/K-th-Not-Divisible-by-n/Program.cs
@@ -1,8 +1,37 @@
-﻿int divisor = Convert.ToInt32(Console.ReadLine());
-int position = Convert.ToInt32(Console.ReadLine());
+﻿int testCase = Convert.ToInt32(Console.ReadLine());
 
-void BinarySearch(int divisor, int position)
+while (testCase != 0)
 {
-    int left = 0, right = position, mid = (left + right)/2;
-
+    var values = Console.ReadLine()!.Split().Select(long.Parse).ToArray();
+    long divisor = values[0];
+    long position = values[1];
+    Find(divisor, position);
+    testCase--;
 }
+
+void Find(long divisor, long position)
+{
+    long left = 0, right = position * divisor;
+    while (right >= left)
+    {
+        long mid = (left + right) / 2;
+        long positionBasedOnMid = (long) Math.Ceiling(mid - (mid/(double)divisor));
+
+        if (positionBasedOnMid == position && mid % divisor != 0)
+        {
+            Console.WriteLine(mid);
+            break;
+        }
+        else if (positionBasedOnMid < position)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1352/C
+// time-complexity -> O(log n)
