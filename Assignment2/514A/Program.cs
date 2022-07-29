19e4553d0365bbfb3b9e4d6442820de2a787e33c
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-07-29 21:54:42 +0600

    solved eighth problem

---
+++ Assignment2/514A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+char[] integers = Console.ReadLine()!.ToCharArray();
+StringBuilder output = new StringBuilder();
+
+for (int cursor = 0; cursor < integers.Length; cursor++)
+{
+    int numInt = (int)char.GetNumericValue(integers[cursor]);
+    int invert = 9 - numInt;
+    if (invert < numInt && (cursor != 0 || invert > 0)) output.Append(invert);
+    else output.Append(numInt);
+}
+Console.WriteLine(output.ToString());
+
+// problem-link -> https://codeforces.com/problemset/problem/514/A
+// time-complexity -> O(n)
