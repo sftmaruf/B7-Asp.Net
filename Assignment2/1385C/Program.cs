975b3c6c5bc001d0ddc3be5d9490fdc0d7b098c7
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-07 20:52:13 +0600

    solved fourteenth problem

---
+++ Assignment2/1385C/Program.cs
@@ -0,0 +1,31 @@
+﻿var NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (NumberOfTestCase-- > 0)
+{
+    var length = Convert.ToInt32(Console.ReadLine());
+    var integers = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    var erasedCount = 0;
+
+    if (length == 1)
+    {
+        Console.WriteLine(erasedCount);
+        continue;
+    }
+
+    bool flag = false;
+    for (var i = integers.Length - 2; i >= 0; i--)
+    {
+        if (!flag && integers[i] < integers[i + 1])
+        {
+            flag = true;
+            continue;
+        }
+
+        if (flag && integers[i] > integers[i + 1])
+        {
+            erasedCount = i + 1;
+            break;
+        }
+    }
+    Console.WriteLine(erasedCount);
+}
