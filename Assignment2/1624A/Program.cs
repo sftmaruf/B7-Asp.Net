20645fa7e0e5e57851da9453301c2168edb11387
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-09 11:21:50 +0600

    solved sixteenth problem

---
+++ Assignment2/1624A/Program.cs
@@ -0,0 +1,11 @@
+﻿var NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while(NumberOfTestCase-- > 0)
+{
+    var length = Convert.ToInt32(Console.ReadLine());
+    var arr = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    Console.WriteLine(arr.Max() - arr.Min());
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1624/A
+// time-complexity -> O(1)
