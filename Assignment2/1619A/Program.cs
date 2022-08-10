4dcee4de1ac7b2602493a7ffc6b50347832644f1
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-11 00:32:23 +0600

    solved eighteenth problem

---
+++ Assignment2/1619A/Program.cs
@@ -0,0 +1,25 @@
+﻿int NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while(NumberOfTestCase-- > 0)
+{
+    var chars = Console.ReadLine()!.ToCharArray();
+    bool isSquare = true;
+
+    if (chars.Length % 2 != 0) isSquare = false;
+    else
+    {
+        int len = chars.Length / 2;
+        for (int i = 0; i < len; i++)
+        {
+            if (chars[i] != chars[len + i])
+            {
+                isSquare = false;
+                break;
+            }
+        }
+    }
+    Console.WriteLine(isSquare ? "YES" : "NO");
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1619/A
+// time-complexity -> O(n/2)
