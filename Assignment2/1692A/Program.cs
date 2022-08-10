5e1ba82fd46aad7cc432a90ec9eb7dceea79b9db
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-10 13:14:06 +0600

    solved seventeenth problem

---
+++ Assignment2/1692A/Program.cs
@@ -0,0 +1,15 @@
+﻿int NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while(NumberOfTestCase-- > 0)
+{
+    int[] distances = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    int count = 0;
+    for (int i = 1; i < distances.Length; i++)
+    {
+        if (distances[0] < distances[i]) count++;
+    }
+    Console.WriteLine(count);
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1692/A
+// time-complexity -> O(n)
