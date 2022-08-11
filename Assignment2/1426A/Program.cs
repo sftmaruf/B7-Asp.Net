da8474eb27e0ea8d42711599028d5d81ca037071
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-11 11:45:47 +0600

    solved nineteenth problem

---
+++ Assignment2/1426A/Program.cs
@@ -0,0 +1,12 @@
+﻿int NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while(NumberOfTestCase-- > 0)
+{
+    int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    int nOfFloor = 1;
+    if (inputs[0] > 2) nOfFloor += (int) Math.Ceiling((inputs[0] - 2.0) / inputs[1]);
+    Console.WriteLine(nOfFloor);
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1426/A
+// time-complexity -> O(1)
