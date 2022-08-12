6150fbfc90655c951a645561517e0fb2cf05421a
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-12 11:58:37 +0600

    solved twenty-first problem

---
+++ Assignment2/1676A/Program.cs
@@ -0,0 +1,11 @@
+﻿int NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (NumberOfTestCase-- > 0)
+{ 
+    int[] integers = Console.ReadLine()!.Select(i => Convert.ToInt32(i)).ToArray();
+    if (integers[..3].Sum() == integers[3..].Sum()) Console.WriteLine("YES");
+    else Console.WriteLine("NO");
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1676/A
+// time-complexity -> O(1)
