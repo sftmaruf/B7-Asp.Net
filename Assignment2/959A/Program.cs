68145a1ff888b439a9f988e18889b13bab2eb710
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-14 14:11:01 +0600

    solved twenty-fifth problem

---
+++ Assignment2/959A/Program.cs
@@ -0,0 +1,5 @@
+﻿int favInt = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(favInt % 2 == 0 ? "Mahmoud" : "Ehab");
+
+// problem-link -> https://codeforces.com/problemset/problem/959/A
+// time-complexity -> O(1)
