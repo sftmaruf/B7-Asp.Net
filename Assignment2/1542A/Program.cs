82ddcbd11af75ad1e5bc7adbd5ef67c66a09c5c1
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-15 13:06:18 +0600

    solved twenty-eighth problem

---
+++ Assignment2/1542A/Program.cs
@@ -0,0 +1,13 @@
+﻿int NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while(NumberOfTestCase-- > 0)
+{
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] arr = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    int oddCount = arr.Count(item => item % 2 != 0);
+    Console.WriteLine(oddCount == length ? "YES" : "NO");
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1542/A
+// time-complexity -> O(n)
