52d74b2e6768c84b80ff8874b2c86af2ac84fef5
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-16 17:05:38 +0600

    solved twenty-ninth problem

---
+++ Assignment2/1325A/Program.cs
@@ -0,0 +1,10 @@
+﻿int NumberOfTestCase  = Convert.ToInt32(Console.ReadLine());
+
+while(NumberOfTestCase-- > 0)
+{
+    int integer = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"1 {integer - 1}");
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1325/A
+// time-complecity -> O(1)
