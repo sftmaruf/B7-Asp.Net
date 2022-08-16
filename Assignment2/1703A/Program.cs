7d9affe26d40c16924c7d5af758aa31c080c3f7b
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-16 17:11:48 +0600

    solved thirtieth problem

---
+++ Assignment2/1703A/Program.cs
@@ -0,0 +1,10 @@
+﻿int NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while(NumberOfTestCase-- > 0)
+{
+    string str = Console.ReadLine()!;
+    Console.WriteLine(str.ToLower() == "yes" ? "YES" : "NO");
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1703/A
+// time-complexity -> O(n)
