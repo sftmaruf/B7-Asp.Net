460b6cd659e735695902fabb9d0316876e239433
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-16 17:50:32 +0600

    solved thirty-first problem

---
+++ Assignment2/1097A/Program.cs
@@ -0,0 +1,15 @@
+﻿char[] CardOnTable = Console.ReadLine()!.ToCharArray();
+string[] CardsOnHand = Console.ReadLine()!.Split().ToArray();
+
+foreach(string card in CardsOnHand)
+{
+    if (card[0] == CardOnTable[0] || card[1] == CardOnTable[1])
+    {
+        Console.WriteLine("YES");
+        return;
+    }
+}
+Console.WriteLine("NO");
+
+// problem-link -> https://codeforces.com/problemset/problem/1097/A
+// time-complexity -> O(1)
