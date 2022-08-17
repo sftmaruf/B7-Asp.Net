8b69f60fc35c13f556b6cd6be79ad21b57089a55
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-17 07:32:41 +0600

    solved thirty-third problem

---
+++ Assignment2/1325B/Program.cs
@@ -0,0 +1,11 @@
+﻿int NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while(NumberOfTestCase-- > 0)
+{
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] elements = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    Console.WriteLine(elements.Distinct().Count());
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1325/B
+// time-complexity -> O(n)
