ac6976612649067eb52c1a4d6449ac89d7fd7e36
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-17 07:09:27 +0600

    solved thirty-second problem

---
+++ Assignment2/1389A/Program.cs
@@ -0,0 +1,12 @@
+﻿int NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (NumberOfTestCase-- > 0)
+{
+    int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    if(inputs[0] * 2 <= inputs[1]) Console.WriteLine($"{inputs[0]} {inputs[0] * 2}");
+    else Console.WriteLine("-1 -1");
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1389/A
+// time-complexity -> O(1)
