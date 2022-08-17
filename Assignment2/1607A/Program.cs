41e1d9a9dfc64c5a9dcf43ccbb8d16193e1be887
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-17 08:02:36 +0600

    solved thirty-fourth problem

---
+++ Assignment2/1607A/Program.cs
@@ -0,0 +1,20 @@
+﻿int NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (NumberOfTestCase-- > 0)
+{
+    string lettersInKeyboard = Console.ReadLine()!;
+    string lettersInString = Console.ReadLine()!;
+
+    int sum = 0;
+    for (int i = 1; i < lettersInString.Length; i++)
+    {
+        sum += Math.Abs(
+            lettersInKeyboard.IndexOf(lettersInString[i])
+            - lettersInKeyboard.IndexOf(lettersInString[i - 1])
+            );
+    }
+    Console.WriteLine(sum);
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1607/A
+// time-complexity -> O(n)
