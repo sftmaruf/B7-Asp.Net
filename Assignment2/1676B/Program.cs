98654a0bf56c1d124ba7f5b34a8c97f2be377371
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-18 16:08:04 +0600

    solved thirty-fifth problem

---
+++ Assignment2/1676B/Program.cs
@@ -0,0 +1,15 @@
+﻿int NumberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (NumberOfTestCase-- > 0)
+{
+    int numberOfBox = Convert.ToInt32(Console.ReadLine());
+    int[] quantities = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    int min = quantities.Min();
+    var extraQuantities = quantities.SkipWhile(quantity => quantity == min);
+    int removedQuantity = extraQuantities.Sum() - (extraQuantities.Count() * min);
+    Console.WriteLine(removedQuantity);
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1676/B
+// time-complexity -> O(n)
