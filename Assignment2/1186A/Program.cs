e89a03c701986ccfebfebbe26c6163a40c47fa84
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-20 00:53:25 +0600

    solved forty-first problem

---
+++ Assignment2/1186A/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+Console.WriteLine(Math.Min(inputs[1], inputs[2]) >= inputs[0] ? "Yes" : "No");
+
+// problem-link -> https://codeforces.com/problemset/problem/1186/A
+// time-complexity -> O(1)
