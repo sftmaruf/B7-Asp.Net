e15865ba8bd796d77b3e2abeddab96602a068dee
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-19 16:27:37 +0600

    solved thirty-ninth problem

---
+++ Assignment2/1337A/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while(numberOfTestCase-- > 0)
+{
+    string[] ints = Console.ReadLine()!.Split();
+    Console.WriteLine($"{ints[1]} {ints[2]} {ints[2]}");
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1337/A
+// time-complexity -> O(1)
