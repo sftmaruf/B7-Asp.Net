a0780bfb3b3ff63cb05f73f3c24dc0d793326b96
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-20 01:08:56 +0600

    solved forty-second problem

---
+++ Assignment2/1660A/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine()); 
+
+while(numberOfTestCase-- > 0)
+{
+    int[] quantities = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    if (quantities[0] == 0) Console.WriteLine(1);
+    else Console.WriteLine(quantities[0] + (quantities[1]*2) + 1);
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1660/A
+// time-complexity -> O(1)
