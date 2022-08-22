eba7da2682729663c0af1ce70c4cc4c0b113302a
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-22 17:43:18 +0600

    solved forty-sixth problem

---
+++ Assignment2/1372A/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfTestCase-- > 0)
+{
+    int length = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(string.Join(" ", Enumerable.Repeat(1, length).ToList()));
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1372/A
+// time-complexity -> O(n)
