a7b8daa90308471de2b9bdb8c07c535710bdfd41
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-22 18:27:03 +0600

    solved forty-seventh problem

---
+++ Assignment2/1650A/Program.cs
@@ -0,0 +1,24 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfTestCase-- > 0)
+{
+    string str = Console.ReadLine()!;
+    char c = Convert.ToChar(Console.ReadLine()!);
+    bool isConvertible = false;
+
+    if (str.Length % 2 != 0) 
+    {
+        for (int i = 0; i < str.Length; i++)
+        {
+            if(str[i] == c && i % 2 == 0)
+            {
+                isConvertible = true;
+                break;
+            }
+        }
+    }
+    Console.WriteLine(isConvertible ? "Yes" : "No");
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1650/A
+// time-complexity -> O(n)
