3775668c042908ad161ad164a3f990799bf04354
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-22 17:34:37 +0600

    solved forty-fourth problem

---
+++ Assignment2/1368A/Program.cs
@@ -0,0 +1,18 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfTestCase-- > 0)
+{
+    int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    int numberOfOperations = 0;
+
+    while (inputs[0] <= inputs[2] && inputs[1] <= inputs[2])
+    {
+        if (inputs[0] > inputs[1]) inputs[1] += inputs[0];
+        else inputs[0] += inputs[1];
+        numberOfOperations++;
+    }
+    Console.WriteLine(numberOfOperations);
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1368/A
+// time-complexity -> O(logn)
