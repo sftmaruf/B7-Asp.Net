db83a3b9793892f42b0cbeee5139b77ebfe9feb9
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-22 19:52:55 +0600

    solved forty-eighth problem

---
+++ Assignment2/1473A/Program.cs
@@ -0,0 +1,21 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfTestCase-- > 0)
+{
+    int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    IList<int> arr = Console.ReadLine()!.Split().Select(int.Parse).ToList();
+    bool isPossible = false;
+
+    if(arr.Max() <= inputs[1]) isPossible = true;
+    else
+    {
+        int min = arr.Min();
+        arr.Remove(min);
+        if (min + arr.Min() <= inputs[1]) isPossible = true;
+    }
+
+    Console.WriteLine(isPossible ? "Yes" : "No");
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1473/A
+// time-complexity -> O(n)
