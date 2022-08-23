1ead671f94fe232a123d99a3055a5e4d5b1443af
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-23 18:43:09 +0600

    solved fifty-first problem

---
+++ Assignment2/978B/Program.cs
@@ -0,0 +1,20 @@
+﻿int length = Convert.ToInt32(Console.ReadLine());
+string str = Console.ReadLine()!;
+
+int countX = 0;
+int removedCount = 0;
+foreach(var ch in str.ToCharArray())
+{
+    if(ch == 'x') countX++;
+    else
+    {
+        if (countX >= 3) removedCount += countX - 2;
+        countX = 0;
+    }
+}
+
+if(countX >= 3) removedCount += countX - 2;
+Console.WriteLine(removedCount);
+
+// problem-link -> https://codeforces.com/problemset/problem/978/B
+// time-complexity -> O(n)
