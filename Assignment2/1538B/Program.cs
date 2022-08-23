8e919966190fcabc9c3049d7e14bcdd538e5baf3
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-23 16:12:03 +0600

    solved fitieth problem

---
+++ Assignment2/1538B/Program.cs
@@ -0,0 +1,18 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfTestCase-- > 0)
+{
+    int nOfFriends = Convert.ToInt32(Console.ReadLine());
+    int[] candies = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    int sum = candies.Sum();
+    if (sum % nOfFriends != 0) Console.WriteLine("-1");
+    else
+    {
+        int mean = sum / nOfFriends;
+        Console.WriteLine(candies.Count(c => c > mean));
+    }
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1538/B
+// time-complexity -> O(n)
