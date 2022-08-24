462d8231c9676e4c8bd333e64b476b5497411fb0
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-25 01:04:57 +0600

    solved fifty-second problem

---
+++ Assignment2/540A/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberOfDisks = Convert.ToInt32(Console.ReadLine());
+int[] diskPositions = Console.ReadLine()!.Select(x => (int) char.GetNumericValue(x)).ToArray();
+int[] secretPositions = Console.ReadLine()!.Select(x => (int)char.GetNumericValue(x)).ToArray();
+
+int totalMoves = 0;
+for (int i = 0; i < numberOfDisks; i++)
+{
+    int dis = Math.Abs(diskPositions[i] - secretPositions[i]);
+    if(dis > 5)
+    {
+        dis = 9 - Math.Max(diskPositions[i], secretPositions[i]);
+        dis += Math.Min(diskPositions[i], secretPositions[i]);
+        dis++;
+    }
+    totalMoves += dis;
+}
+Console.WriteLine(totalMoves);
+
+// problem-link -> https://codeforces.com/problemset/problem/540/A
+// time-complexity -> O(n)
