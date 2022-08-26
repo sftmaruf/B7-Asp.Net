ca2279e0c3bd6b5ee13cc09051cfb2acb5caa94d
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-27 01:49:12 +0600

    solved fifty-fourth problem

---
+++ Assignment2/1490A/Program.cs
@@ -0,0 +1,25 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfTestCase-- > 0)
+{
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] ints = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    int requiredQuantity = 0;
+    for (int i = 1; i < length;)
+    {
+        int max = Math.Max(ints[i], ints[i - 1]);
+        int min = Math.Min(ints[i], ints[i - 1]);
+        if (max / (double)min > 2.0)
+        {
+            if (ints[i] > ints[i - 1]) ints[i - 1] = min * 2;
+            if (ints[i] < ints[i - 1]) ints[i - 1] = (int) Math.Ceiling(max / 2.0);
+            requiredQuantity++;
+        }
+        else i++;
+    }
+    Console.WriteLine(requiredQuantity);
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1490/A
+// time-complexity -> O(n)
