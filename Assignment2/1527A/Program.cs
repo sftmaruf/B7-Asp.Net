f5f4e1bfe935efa48d1617ef6b48e7860954f022
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-27 00:27:32 +0600

    solved fifty-third problem

---
+++ Assignment2/1527A/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfTestCase-- > 0)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    int required;
+
+    if (number <= 2) required = number;
+    else
+    {
+        required = 4;
+        while(number >= required) required *= 2;
+        required /= 2;
+    }
+    Console.WriteLine(required - 1);
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1527/A
+// time-complexity -> O(logn)
