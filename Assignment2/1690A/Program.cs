e383bf061a9bb09ec03f7cbb0b16653fc4af6dcd
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-27 17:01:03 +0600

    solved fifty-fifth problem

---
+++ Assignment2/1690A/Program.cs
@@ -0,0 +1,25 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfTestCase-- > 0)
+{
+    int totalBlocks = Convert.ToInt32(Console.ReadLine());
+    int[] heights = new int[3];
+    heights[0] = heights[1] = heights[2] = totalBlocks / 3;
+
+    if (heights[0] * 3 != totalBlocks)
+    {
+        if (totalBlocks - (heights[0] * 3) == 2) heights[0]++;
+        heights[1] += 2;
+        heights[2]--;
+    }
+    else
+    {
+        heights[1]++;
+        heights[2]--;
+    }
+
+    Console.WriteLine(string.Join(" ", heights));
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1690/A
+// time-complexity -> O(1)
