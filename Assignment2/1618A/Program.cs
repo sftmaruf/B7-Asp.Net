584c5ab9668c481b85f5026ada43dec94a3ac9c6
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-29 10:12:56 +0600

    solved fifty-seventh problem

---
+++ Assignment2/1618A/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfTestCase-- > 0)
+{
+    int[] ints = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    IList<int> results = new List<int>(ints.Take(2))
+    {
+        ints.Last() - (ints[0] + ints[1])
+    };
+    Console.WriteLine(string.Join(" ", results));
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1618/A
+// time-complexity -> O(1)
