6c8f07591f083bf5a353dd20abb88637e57cd4a6
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-29 11:35:22 +0600

    solved fifty-eighth problem

---
+++ Assignment2/1529A/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfTestCase-- > 0)
+{
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] ints = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    int maxDeletedElement = 0;
+    
+    Array.Sort(ints);
+    if (ints.First() != ints.Last())
+    {
+        int minCount = ints.Count(i => i == ints[0]);
+        maxDeletedElement = length - minCount;
+    }
+    Console.WriteLine(maxDeletedElement);
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1529/A
+// time-complexity - O(nlogn)
