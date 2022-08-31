947ee9ee12d93f73d3fbfbf824eb223e452dce1d
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-31 12:16:20 +0600

    solved fifty-ninth problem

---
+++ Assignment2/686A/Program.cs
@@ -0,0 +1,19 @@
+﻿long[] inputs = Console.ReadLine()!.Split().Select(long.Parse).ToArray();
+
+long distressedKids = 0;
+while (inputs[0]-- > 0)
+{
+    string[] signWithQuantity = Console.ReadLine()!.Split();
+    long quantity = long.Parse(signWithQuantity[1]);
+
+    if (signWithQuantity[0] == "+") inputs[1] += quantity;
+    else
+    {
+        if (inputs[1] < quantity) distressedKids++;
+        else inputs[1] -= quantity;
+    }
+}
+Console.WriteLine($"{inputs[1]} {distressedKids}");
+
+// problem-link -> https://codeforces.com/problemset/problem/686/A
+// time-complexity -> O(n)
