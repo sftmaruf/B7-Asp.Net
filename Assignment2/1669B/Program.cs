ef0a2f2b316fd8a94a8747087c0bbc46ead9a012
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-31 17:32:43 +0600

    solved sixty-second problem

---
+++ Assignment2/1669B/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while(numberOfTestCase-- > 0)
+{
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] arr = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    int[] result = arr.GroupBy(e => e).Where(e => e.Count() >= 3)
+        .Select(e => e.Key).ToArray();
+    Console.WriteLine(result.Length >= 1 ? result[0] : -1);
+}
+
+// problem-linl -> https://codeforces.com/problemset/problem/1669/B
+// time-complexity -> O(n)
