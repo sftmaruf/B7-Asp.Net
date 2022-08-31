3b6fd2fe1187b00009f2d8f6e9d99cb0d6784dbe
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-31 20:38:25 +0600

    solved sixty-fourth problem

---
+++ Assignment2/711A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+int rows = Convert.ToInt32(Console.ReadLine());
+
+bool isPossible = false;
+StringBuilder busConfig = new StringBuilder();
+
+for(int i = 0; i < rows; i++)
+{
+    string seatConfig = Console.ReadLine()!;
+    if (!isPossible)
+    {
+        isPossible = seatConfig.Contains("OO");
+        if(isPossible)
+        {
+            string[] seats = seatConfig.Split('|');
+            if (seats[0].Contains("OO"))
+            {
+                seatConfig = seatConfig.Replace("OO|", "++|");
+            }
+            else
+            {
+                seatConfig = seatConfig.Replace("|OO", "|++");
+            }
+        }
+    }
+    busConfig.Append(seatConfig).Append('\n');
+}
+Console.WriteLine(isPossible ? "YES" : "NO");
+if(isPossible) Console.WriteLine(busConfig.ToString());
+
+// problem-link -> https://codeforces.com/problemset/problem/711/A
+// time-complexity -> O(n)
