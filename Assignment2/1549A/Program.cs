188c69168f97ca89ebf34569da4ab6dfd5514d50
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-31 20:15:06 +0600

    solved sixty-third problem

---
+++ Assignment2/1549A/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while(numberOfTestCase-- > 0)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{2} {number-1}");
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1549/A
+// time-complexity -> O(1)
