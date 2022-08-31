bbe5ac164a799584ce62fa1dccb2e26b7d7892ee
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-08-31 12:49:59 +0600

    solved sixtieth problem

---
+++ Assignment2/1633A/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberOfTestCase = Convert.ToInt32(Console.ReadLine());
+
+while(numberOfTestCase-- > 0)
+{
+    int result;
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number % 7 == 0) result = number;
+    else
+    {
+        int remainder = number % 7;
+        if (number % 10 >= remainder) result = number - remainder;
+        else result = number + (7 - remainder);
+    }
+    Console.WriteLine(result);
+}
+
+// problem-link -> https://codeforces.com/problemset/problem/1633/A
+// time-complexity -> O(1)
