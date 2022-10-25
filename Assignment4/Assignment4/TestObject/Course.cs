b75630368ed0f26350e6409ab401920ac9f23d63
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-10-25 19:41:24 +0600

    added test objects

--- Assignment4/Assignment4/TestObject/Course.cs
+++ Assignment4/Assignment4/TestObject/Course.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assignment4.TestObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -8,16 +9,54 @@ using System.Threading.Tasks;
 
 namespace Assignment4.TestObject
 {
-    public class Course : IEntity
+    public class Course : IEntity<int>
     {
-        public Guid Id { get; }
-        public string? Name { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
         public Instructor Teacher { get; set; }
+        public List<Topic> Topics { get; set; }
+        public double Fees { get; set; }
+        public List<AdmissionTest> Tests { get; set; }
+
+    }
+    public class AdmissionTest
+    {
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public double TestFees { get; set; }
     }
 
+    public class Topic
+    {
+        public Guid Id { get; set;  }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Session> Sessions { get; set; }
+    }
+    public class Session
+    {
+        public int DurationInHour { get; set; }
+        public string LearningObjective { get; set; }
+    }
     public class Instructor
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public Address PresentAddress { get; set; }
+        public Address PermanentAddress { get; set; }
+        public List<Phone> PhoneNumbers { get; set; }
+    }
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+    public class Phone
+    {
+        public string Number { get; set; }
+        public string Extension { get; set; }
+        public string CountryCode { get; set; }
     }
 }
