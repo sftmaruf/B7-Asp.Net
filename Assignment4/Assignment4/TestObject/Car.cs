0db6cef9b7dd2d9ed861cfeab4d6edf7d9fa3c0f
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-10-25 19:40:59 +0600

    added test objects

---
+++ Assignment4/Assignment4/TestObject/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4.TestObject
+{
+    public class Car : IEntity<int>
+    {
+        public int Id { get; set; } 
+        public Brand Brand;
+        public string Model;
+        public double Price;
+        public List<Wheel> Wheels;
+        public List<Door> Door;
+    }
+
+    public class Wheel
+    {
+        public int Id { get; set; }
+        public Brand Brand;
+    }
+
+    public class Door : IEntity<int>
+    {
+        public int Id { get; set; }
+        public Brand Brand;
+    }
+
+    public class Brand
+    {
+        public string Name;
+        public string Country;
+    }
+}
