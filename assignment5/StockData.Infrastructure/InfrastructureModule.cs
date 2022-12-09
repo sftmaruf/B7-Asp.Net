de2df444b0f42d338dc10ef5b3d61404a33fc1bb
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-12-09 15:48:57 +0600

    added Infrastructure module

---
+++ assignment5/StockData.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using StockData.Infrastructure.DbContexts;
+using StockData.Infrastructure.Repositories;
+using StockData.Infrastructure.Services;
+using StockData.Infrastructure.UnitOfWorks;
+
+namespace StockData.Infrastructure
+{
+    public class InfrastructureModule : Module
+    {
+        private readonly string _connectionString;
+        private readonly string _assemblyName;
+
+        public InfrastructureModule(string connectionString, string assemblyName)
+        {
+            _connectionString = connectionString;
+            _assemblyName = assemblyName;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ApplicationDbContext>()
+                .AsSelf()
+                .WithParameter("connectionString", _connectionString)
+                .WithParameter("assemblyName", _assemblyName)
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<ApplicationDbContext>()
+                .As<IApplicationDbContext>()
+                .WithParameter("connectionString", _connectionString)
+                .WithParameter("assemblyName", _assemblyName)
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<CompanyRepository>()
+                .As<ICompanyRepository>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<ApplicationUnitOfWork>()
+                .As<IApplicationUnitOfWork>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<StockService>()
+                .As<IStockService>()
+                .InstancePerLifetimeScope();
+
+            base.Load(builder);
+        }
+    }
+}
