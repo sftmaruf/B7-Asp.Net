533dfb95231624975de8336c002236c662993b7d
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-12-09 15:45:46 +0600

    added Entities folder and IEntity, Company, StockPrice entity classes

---
+++ assignment5/StockData.Infrastructure/Entities/StockPrice.cs
@@ -0,0 +1,20 @@
+﻿using StockData.Infrastructure.Entities;
+
+namespace StockData.Infrastructure.Entities
+{
+    public class StockPrice : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public double LastTradingPrice { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double ClosePrice { get; set; }
+        public double YesterdayClosePrice { get; set; }
+        public string Change { get; set; }
+        public string Trade { get; set; }
+        public string Value { get; set; }
+        public string Volume { get; set; }
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
