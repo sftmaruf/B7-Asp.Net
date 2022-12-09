e5485a6ff81a5ea5e74249e62987677150235501
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-12-09 15:47:03 +0600

    added Repositories folder and Repository, IRepository, ICompanyRepository, CompanyRepository classes

---
+++ assignment5/StockData.Infrastructure/Repositories/CompanyRepository.cs
@@ -0,0 +1,15 @@
+﻿using FirstDemo.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using StockData.Infrastructure.DbContexts;
+using StockData.Infrastructure.Entities;
+
+namespace StockData.Infrastructure.Repositories
+{
+    public class CompanyRepository : Repository<Company, Guid>, ICompanyRepository
+    {
+        public CompanyRepository(IApplicationDbContext applicationDbContext) :
+            base((DbContext) applicationDbContext)
+        {
+        }
+    }
+}
---
+++ assignment5/StockData.Infrastructure/Repositories/ICompanyRepository.cs
@@ -0,0 +1,9 @@
+﻿using FirstDemo.Infrastructure.Repositories;
+using StockData.Infrastructure.Entities;
+
+namespace StockData.Infrastructure.Repositories
+{
+    public interface ICompanyRepository : IRepository<Company, Guid>
+    {
+    }
+}
