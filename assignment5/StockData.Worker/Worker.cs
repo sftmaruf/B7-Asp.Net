5745cdfdee5cb406dacf0ce35628ab139141fa69
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-12-09 15:59:38 +0600

    Dependency injected ILifeTimeScope, moved all the monolithic code into the model and service

--- assignment5/StockData.Worker/Worker.cs
+++ assignment5/StockData.Worker/Worker.cs
@@ -1,4 +1,10 @@
+using Autofac;
 using HtmlAgilityPack;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using StockData.Infrastructure.Entities;
+using StockData.Infrastructure.UnitOfWorks;
+using StockData.Worker.Models;
+using System.Diagnostics;
 using System.Text;
 
 namespace StockData.Worker
@@ -6,10 +12,12 @@ namespace StockData.Worker
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly ILifetimeScope _scope;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, ILifetimeScope scope)
         {
             _logger = logger;
+            _scope = scope;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -19,76 +27,7 @@ namespace StockData.Worker
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
 
-                var url = "https://www.dse.com.bd/latest_share_price_scroll_l.php";
-                var web = new HtmlWeb();
-                var doc = web.Load(url);
-
-                string statusClose = "Closed";
-
-                var marketStatus = doc.DocumentNode
-                    .SelectSingleNode("//header/div")
-                    .ChildNodes[7]
-                    .SelectSingleNode("//span/b").InnerHtml;
-
-                if (marketStatus != statusClose)
-                {
-                    //Console.WriteLine(doc.ParsedText);
-                }
-                else
-                {
-                    var nodes = doc.DocumentNode.SelectSingleNode("//div[@id='RightBody']")
-                        .ChildNodes[5]
-                        .ChildNodes[3]
-                        .ChildNodes[1]
-                        .ChildNodes[1]
-                        .ChildNodes;
-
-                    foreach (var node in nodes)
-                    {
-                        if(node.Name == "tbody")
-                        {
-                            StringBuilder builder = new StringBuilder();
-                            var tempNodes = node.ChildNodes[1].ChildNodes;
-                            foreach(var n in tempNodes)
-                            {
-                                if(n.Descendants("a").Any())
-                                {
-                                    builder
-                                        .Append(n.Descendants("a").ToList()[0].InnerHtml.Trim())
-                                        .Append(" ");
-                                }
-                                else
-                                {
-                                    builder.Append(n.InnerHtml.Trim());
-                                    builder.Append(" ");
-                                }
-                            }
-                            
-                            Console.WriteLine(builder.ToString());
-                        }
-
-                        if (node.Name == "tr")
-                        {
-                            StringBuilder builder = new StringBuilder();
-                            var tempNodes = node.ChildNodes;
-                            foreach (var n in tempNodes)
-                            {
-                                if (n.Descendants("a").Any())
-                                {
-                                    builder.Append(n.Descendants("a").ToList()[0].InnerHtml.Trim());
-                                    builder.Append(" ");
-                                }
-                                else
-                                {
-                                    builder.Append(n.InnerHtml.Trim());
-                                    builder.Append(" ");
-                                }
-                            }
-
-                            Console.WriteLine(builder.ToString());
-                        }
-                    }
-                }
+                _scope.Resolve<StockDataModel>().LoadData();
 
                 await Task.Delay(60000, stoppingToken);
             }
