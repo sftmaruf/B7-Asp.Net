88c789b73726594e1940091ed27c6f6338d7d68d
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-12-10 23:57:16 +0600

    added new migration with a new column - TimeStamp

--- assignment5/StockData.Worker/Migrations/20221210174136_AddedTimeStamp.Designer.cs
+++ assignment5/StockData.Worker/Migrations/20221210174136_AddedTimeStamp.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StockData.Infrastructure.DbContexts;
 
@@ -11,9 +12,10 @@ using StockData.Infrastructure.DbContexts;
 namespace StockData.Worker.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221210174136_AddedTimeStamp")]
+    partial class AddedTimeStamp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ assignment5/StockData.Worker/Migrations/20221210174136_AddedTimeStamp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockData.Worker.Migrations
+{
+    public partial class AddedTimeStamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TimeStamp",
+                table: "StockPrice",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeStamp",
+                table: "StockPrice");
+        }
+    }
+}
