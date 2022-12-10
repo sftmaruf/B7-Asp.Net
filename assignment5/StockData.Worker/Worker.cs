481d72419c2425f98dfa392f4a4a7ce41fbd89cc
Shafayet Maruf <shafayetmaruf@gmail.com>
2022-12-10 23:55:03 +0600

    removed unnecessary log information

--- assignment5/StockData.Worker/Worker.cs
+++ assignment5/StockData.Worker/Worker.cs
@@ -1,11 +1,5 @@
 using Autofac;
-using HtmlAgilityPack;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using StockData.Infrastructure.Entities;
-using StockData.Infrastructure.UnitOfWorks;
 using StockData.Worker.Models;
-using System.Diagnostics;
-using System.Text;
 
 namespace StockData.Worker
 {
@@ -24,9 +18,6 @@ namespace StockData.Worker
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-
                 _scope.Resolve<StockDataModel>().LoadData();
 
                 await Task.Delay(60000, stoppingToken);
