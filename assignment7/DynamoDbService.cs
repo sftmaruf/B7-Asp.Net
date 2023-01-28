67bb8db47b0b7dfd210adbe3251292cbd43a5eda
Shafayet Maruf <shafayetmaruf@gmail.com>
2023-01-28 23:42:51 +0600

    created service for the DynamoDb

---
+++ assignment7/DynamoDbService.cs
@@ -0,0 +1,118 @@
+﻿using Amazon.DynamoDBv2;
+using assignment7.Entities;
+using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace assignment7
+{
+    public class DynamoDbService<T> : AwsService
+    {
+        private readonly string dbName;
+        private readonly AmazonDynamoDBClient _client;
+        private readonly DynamoDBContext _context;
+
+        public DynamoDbService() : this("aspnetb7-shafayet-assignment7")
+        {
+        }
+
+        public DynamoDbService(string name)
+        {
+            dbName = name;
+            _client = new AmazonDynamoDBClient(_accessKey, _secretKey, _region);
+            _context = new DynamoDBContext(_client);
+        }
+
+        public async Task AddDemoAsync()
+        {
+            var persons = Person.DemoData();
+
+            await AddAsync((IList<T>)persons);
+           
+        }
+            
+        public async Task AddAsync(IList<T> persons)
+        {
+            var batch = _context.CreateBatchWrite<T>(new DynamoDBOperationConfig
+            {
+                OverrideTableName = dbName
+            });
+
+            batch.AddPutItems(persons);
+            Console.WriteLine($"Added {persons.Count} data into the Database");
+            await batch.ExecuteAsync();
+        }
+
+        public async Task DeleteAsync(string hashKey, string rangeKey)
+        {
+            if(!await IsExist(hashKey, rangeKey))
+            {
+                Console.WriteLine($"{hashKey} doesn't exist in the database");
+                return;
+            }
+
+            await _context.DeleteAsync<T>(hashKey, rangeKey, new DynamoDBOperationConfig
+            {
+                OverrideTableName = dbName
+            });
+            
+            if(!await IsExist(hashKey, rangeKey))
+            {
+                Console.WriteLine($"'{hashKey}' deleted successfully");
+            }
+        }
+
+        private async Task<bool> IsExist(string hashKey, string rangeKey)
+        {
+            try
+            {
+                var deletedItem = await LoadDataAsync(hashKey, rangeKey);
+                if (deletedItem != null) return true;
+            }
+            catch(AmazonDynamoDBException exception)
+            {
+                if(exception.ErrorCode != "ResourceNotFoundException")
+                    throw;
+            }
+            
+            return false;
+        }
+
+        private async Task<T> LoadDataAsync(string hashKey, string rangeKey)
+        {
+            return await _context.LoadAsync<T>(hashKey, rangeKey, new DynamoDBOperationConfig
+            {
+                OverrideTableName = dbName
+            });
+        }
+
+        public async Task <List<T>> LoadTenDataAsync()
+        {
+            return await LoadDataAsync(10);
+        }
+
+        public async Task<List<T>> LoadDataAsync(int range)
+        {
+            var request = new ScanRequest
+            {
+                TableName = dbName,
+                Limit = range
+            };
+
+            var response = await _client.ScanAsync(request);
+
+            var persons = new List<T>();
+
+            foreach(var record in response.Items)
+            {
+                var person = Activator.CreateInstance(typeof(T));
+                foreach (var r in record)
+                {
+                    person?.GetType()?.GetProperty(r.Key)?.SetValue(person, r.Value.S);
+                }
+                persons.Add((T)person!);
+            }
+
+            return persons;
+        }
+    }
+}
---
+++ assignment7/Entities/Person.cs
@@ -0,0 +1,70 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace assignment7.Entities
+{
+    public class Person
+    {
+        [DynamoDBHashKey]
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public static IList<Person> DemoData()
+        {
+            var persons = new List<Person>
+            {
+                new Person
+                {
+                    Name = "Name0",
+                    PhoneNumber = "01710000000"
+                },
+                new Person
+                {
+                    Name = "Name1",
+                    PhoneNumber = "01710000001"
+                },
+                new Person
+                {
+                    Name = "Name2",
+                    PhoneNumber = "01710000002"
+                },
+                new Person
+                {
+                    Name = "Name3",
+                    PhoneNumber = "01710000003"
+                },
+                new Person
+                {
+                    Name = "Name4",
+                    PhoneNumber = "01710000004"
+                },
+                new Person
+                {
+                    Name = "Name5",
+                    PhoneNumber = "01710000005"
+                },
+                new Person
+                {
+                    Name = "Name6",
+                    PhoneNumber = "01710000006"
+                },
+                new Person
+                {
+                    Name = "Name7",
+                    PhoneNumber = "01710000007"
+                },
+                new Person
+                {
+                    Name = "Name8",
+                    PhoneNumber = "01710000008"
+                },
+                new Person
+                {
+                    Name = "Name9",
+                    PhoneNumber = "01710000009"
+                }
+            };
+
+            return persons;
+        }
+    }
+}
