89f35a2fcb4c7c44d545b09a17c542dbe5abd41e
Shafayet Maruf <shafayetmaruf@gmail.com>
2023-01-28 23:43:30 +0600

    created a base service

---
+++ assignment7/AwsService.cs
@@ -0,0 +1,22 @@
+﻿using Amazon;
+using Amazon.Runtime.CredentialManagement;
+
+namespace assignment7
+{
+    public abstract class AwsService
+    {
+        protected readonly string _accessKey;
+        protected readonly string _secretKey;
+        protected readonly RegionEndpoint _region;
+
+        public AwsService()
+        {
+            var credential = new NetSDKCredentialsFile();
+            credential.TryGetProfile("assignment7", out var profile);
+
+            _accessKey = profile.Options.AccessKey;
+            _secretKey = profile.Options.SecretKey;
+            _region = profile.Region;
+        }
+    }
+}
